15fefb3db83e04e7837bb66e16de4b1f2b85e8ea
leung25 <leung25@llnl.gov>
2013-12-20 12:12:47 -0800

    added the diag path in urls.py

--- apps/uweb/uvdjango/urls.py
+++ apps/uweb/uvdjango/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
                                     {'document_root': settings.MEDIA_ROOT}),
                        (r'', include('home.urls')),
                        (r'^plots/', include('home.urls')),
+                       (r'^diags/', include('diag.urls')),
                        (r'^login/', include('login.urls', namespace='login')),
     # Examples:
     # url(r'^$', 'uvdjango.views.home', name='home'),
