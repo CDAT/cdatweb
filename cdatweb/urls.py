ebc66e3dcea5ea4077705e458bf3deddc1f5ad6e
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-18 15:34:57 -0500

    Added a default view rendering base.html

--- cdatweb/urls.py
+++ cdatweb/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from . import views
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
 )
 
 # Development
