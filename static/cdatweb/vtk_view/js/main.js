73a31bcfcb19bd660153ed380493e86f2b34741d
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-01-20 11:32:04 -0500

    Start client code to render lists of variables

--- static/cdatweb/vtk_view/js/main.js
+++ static/cdatweb/vtk_view/js/main.js
@@ -1,15 +1,49 @@
-window.app = window.app || {};
+(function () {
+    window.cdat = window.cdat || {};
+    var app = window.cdat;
 
-app.main = function (connection) {
-    // default!?
-};
+    function renderVariables(connection, variables) {
+    }
 
-app.error = function (msg) {
-    // TODO: create error page
-    console.error(msg);
-};
+    function renderBrowser(connection, files) {
+        $('.vtk-file-browser').fileBrowser({
+            data: [files],
+            session: connection.session
+        })
+        .bind('file-click',/* directory-click directory-not-found file-group-click'*/ function (e) {
+            // e.type, e.name, e.path, e.relativePathList
 
-app.variablesPanel = function (filename) {
-    // list all variables in the given file
+            if (e.relativePathList.length) {
+                connection.sessions
+                    .call('file.server.info', e.relativePathList[0])
+                    .then(function (variables) {
+                        renderVariables(connection, variables);
+                    });
+            }
 
-};
+        });
+    }
+
+    app.main = function (connection) {
+        // default!?
+    };
+
+    app.error = function (msg) {
+        // TODO: create error page
+        console.error(msg);
+    };
+
+    app.variables = function () {
+        // list all variables in the given file
+
+    };
+
+    app.browser = function (connection) {
+        // connect the vtkweb file browser widget
+        connection.session
+            .call('file.server.directory.list', ['.'])
+            .then(function (files) {
+                renderBrowser(connection, files);
+            });
+    };
+})();
