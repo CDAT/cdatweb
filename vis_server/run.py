df82dde3ec4d5602d0037f08a29343363b813443
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-05-28 09:08:16 -0400

    Fix pep8 errors

--- vis_server/run.py
+++ vis_server/run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 import sys
 import os
@@ -5,16 +6,19 @@ import argparse
 
 import vtk
 
-sys.path.append(os.path.dirname(vtk.__file__))
+sys.path.append(os.path.dirname(vtk.__file__))  # noqa
 
 from vtk.web import server
 from vtk.web import wamp
 
+import protocols
 from protocols.readers import cdms_reader
+import vcs
 
 from external import exportRpc
 import settings
 _viewers = []
+
 
 class CDATWebVisualizer(wamp.ServerProtocol):
 
@@ -54,94 +58,54 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    settings.SERVER_TEST=args.testing
+    settings.SERVER_TEST = args.testing
 
     CDATWebVisualizer.uploadPath = args.uploadPath
 
-import protocols
-if not args.testing:
-    import vcs
-    import cdms2
-    class TestProtocol(protocols.BaseProtocol):
-        _open_views = {}
-        _dirty_views = {}
 
-        @exportRpc('cdat.view.create')
-        def create_view(self, fname, varname, opts={}):
-            reader = cdms_reader.Cdms_reader(fname)
-            v = reader.read(varname)
-            canvas = vcs.init()
-            canvas.setbgoutputdimensions(width=500, height=500, units='pixels')
-            plot = canvas.plot(
-                v
-            )
-            window = canvas.backend.renWin
-            id = self.getGlobalId(window)
-            self._open_views[id] = (
-                window,
-                canvas
-            )
+class TestProtocol(protocols.BaseProtocol):
+    _open_views = {}
+    _dirty_views = {}
 
-            def dirty(*arg, **kw):
-                self._dirty_views[id] = True
+    @exportRpc('cdat.view.create')
+    def create_view(self, fname, varname, opts={}):
+        reader = cdms_reader.Cdms_reader(fname)
+        v = reader.read(varname)
+        canvas = vcs.init()
+        canvas.setbgoutputdimensions(width=500, height=500, units='pixels')
+        canvas.plot(v)
+        window = canvas.backend.renWin
+        id = self.getGlobalId(window)
+        self._open_views[id] = (
+            window,
+            canvas
+        )
 
-            def resize(*arg, **kw):
-                if self._dirty_views.pop(id, None):
-                    canvas.update()
+        def dirty(*arg, **kw):
+            self._dirty_views[id] = True
 
-            window.AddObserver(vtk.vtkCommand.ModifiedEvent, dirty)
-            window.AddObserver(vtk.vtkCommand.EndEvent, resize)
+        def resize(*arg, **kw):
+            if self._dirty_views.pop(id, None):
+                canvas.update()
 
-            return id
+        window.AddObserver(vtk.vtkCommand.ModifiedEvent, dirty)
+        window.AddObserver(vtk.vtkCommand.EndEvent, resize)
 
-        @exportRpc('cdat.view.update')
-        def update_view(self, id):
-            window, canvas = self._open_views[id]
-            canvas.update()
-            window.Render()
+        return id
 
+    @exportRpc('cdat.view.update')
+    def update_view(self, id):
+        window, canvas = self._open_views[id]
+        canvas.update()
+        window.Render()
 
-        @exportRpc('cdat.view.destroy')
-        def destroy_view(self, id):
-            cache = self._open_views.pop(id, None)
-            if cache:
-                cache[1].close()
-                cache[0].Finalize()
-else:
-    class TestProtocol(protocols.BaseProtocol):
-        _open_views = {}
-
-        @exportRpc('cdat.view.create')
-        def create_view(self, *arg, **kw):
-            renderer = vtk.vtkRenderer()
-            renderWindow = vtk.vtkRenderWindow()
-            renderWindow.AddRenderer(renderer)
-
-            renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-            renderWindowInteractor.SetRenderWindow(renderWindow)
-            renderWindowInteractor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
-
-            cone = vtk.vtkConeSource()
-            mapper = vtk.vtkPolyDataMapper()
-            actor = vtk.vtkActor()
-
-            mapper.SetInputConnection(cone.GetOutputPort())
-            actor.SetMapper(mapper)
-
-            renderer.AddActor(actor)
-            renderer.ResetCamera()
-            renderWindow.Render()
-            id = self.getGlobalId(renderWindow)
-            self._open_views[id] = renderWindow
-            return id
-
-        @exportRpc('cdat.view.destroy')
-        def destroy_view(self, id):
-            cache = self._open_views.pop(id, None)
-            if cache:
-                cache.Finalize()
-
+    @exportRpc('cdat.view.destroy')
+    def destroy_view(self, id):
+        cache = self._open_views.pop(id, None)
+        if cache:
+            cache[1].close()
+            cache[0].Finalize()
 
 if __name__ == '__main__':
-    print "CDATWeb Visualization server initializing"
+    print("CDATWeb Visualization server initializing")
     server.start_webserver(options=args, protocol=CDATWebVisualizer)
