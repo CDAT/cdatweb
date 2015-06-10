ded7c394602c2cefd0cbce4cde9fdf59177ba16a
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-06-10 09:57:15 -0400

    Start refactoring vis_server tests

--- vis_server/protocols/readers/__init__.py
+++ vis_server/protocols/readers/__init__.py
@@ -1,11 +1,5 @@
-from settings import SERVER_TEST
-
-if not SERVER_TEST:
-    from cdms_reader import Cdms_reader
-    __all__ = [Cdms_reader]
-else:
-    from testing import TestReader
-    __all__ = [TestReader]
+from cdms_reader import Cdms_reader
+__all__ = [Cdms_reader]
 
 _reader_map = {reader.name: reader for reader in __all__}
 
--- vis_server/run.py
+++ vis_server/run.py
@@ -10,11 +10,58 @@ sys.path.append(os.path.dirname(vtk.__file__))  # noqa
 from vtk.web import server
 from vtk.web import wamp
 
+import vcs
+
 from protocols.readers import cdms_reader
 
 from external import exportRpc
-import settings
+import protocols
+
 _viewers = []
+
+
+class CDatProtocol(protocols.BaseProtocol):
+    _open_views = {}
+    _dirty_views = {}
+
+    @exportRpc('cdat.view.create')
+    def create_view(self, fname, varname, opts={}):
+        reader = cdms_reader.Cdms_reader(fname)
+        v = reader.read(varname)
+        canvas = vcs.init()
+        canvas.setbgoutputdimensions(width=500, height=500, units='pixels')
+        canvas.plot(v)
+        window = canvas.backend.renWin
+        id = self.getGlobalId(window)
+        self._open_views[id] = (
+            window,
+            canvas
+        )
+
+        def dirty(*arg, **kw):
+            self._dirty_views[id] = True
+
+        def resize(*arg, **kw):
+            if self._dirty_views.pop(id, None):
+                canvas.update()
+
+        window.AddObserver(vtk.vtkCommand.ModifiedEvent, dirty)
+        window.AddObserver(vtk.vtkCommand.EndEvent, resize)
+
+        return id
+
+    @exportRpc('cdat.view.update')
+    def update_view(self, id):
+        window, canvas = self._open_views[id]
+        canvas.update()
+        window.Render()
+
+    @exportRpc('cdat.view.destroy')
+    def destroy_view(self, id):
+        cache = self._open_views.pop(id, None)
+        if cache:
+            cache[1].close()
+            cache[0].Finalize()
 
 
 class CDATWebVisualizer(wamp.ServerProtocol):
@@ -23,7 +70,6 @@ class CDATWebVisualizer(wamp.ServerProtocol):
     uploadPath = '.'
 
     def initialize(self):
-
         # intialize protocols
         self.registerVtkWebProtocol(protocols.MouseHandler())
         self.registerVtkWebProtocol(protocols.ViewPort())
@@ -37,7 +83,7 @@ class CDATWebVisualizer(wamp.ServerProtocol):
         self.registerVtkWebProtocol(protocols.FileLoader(self.uploadPath))
         self.registerVtkWebProtocol(protocols.FileFinder(self.uploadPath))
         self.registerVtkWebProtocol(protocols.ViewportDeleter())
-        self.registerVtkWebProtocol(TestProtocol())
+        self.registerVtkWebProtocol(CDatProtocol())
 
 
 if __name__ == '__main__':
@@ -46,101 +92,8 @@ if __name__ == '__main__':
     )
 
     server.add_arguments(parser)
-    parser.add_argument(
-        '--testing',
-        action='store_true',
-        dest='testing',
-        help='Enable testing mode (bypass uvcdat)'
-    )
-
     args = parser.parse_args()
-
-    settings.SERVER_TEST = args.testing
-
     CDATWebVisualizer.uploadPath = args.uploadPath
 
-import protocols  # noqa
-if not args.testing:
-    import vcs  # noqa
-    import cdms2  # noqa
-
-    class TestProtocol(protocols.BaseProtocol):
-        _open_views = {}
-        _dirty_views = {}
-
-        @exportRpc('cdat.view.create')
-        def create_view(self, fname, varname, opts={}):
-            reader = cdms_reader.Cdms_reader(fname)
-            v = reader.read(varname)
-            canvas = vcs.init()
-            canvas.setbgoutputdimensions(width=500, height=500, units='pixels')
-            canvas.plot(v)
-            window = canvas.backend.renWin
-            id = self.getGlobalId(window)
-            self._open_views[id] = (
-                window,
-                canvas
-            )
-
-            def dirty(*arg, **kw):
-                self._dirty_views[id] = True
-
-            def resize(*arg, **kw):
-                if self._dirty_views.pop(id, None):
-                    canvas.update()
-
-            window.AddObserver(vtk.vtkCommand.ModifiedEvent, dirty)
-            window.AddObserver(vtk.vtkCommand.EndEvent, resize)
-
-            return id
-
-        @exportRpc('cdat.view.update')
-        def update_view(self, id):
-            window, canvas = self._open_views[id]
-            canvas.update()
-            window.Render()
-
-        @exportRpc('cdat.view.destroy')
-        def destroy_view(self, id):
-            cache = self._open_views.pop(id, None)
-            if cache:
-                cache[1].close()
-                cache[0].Finalize()
-else:
-    class TestProtocol(protocols.BaseProtocol):
-        _open_views = {}
-
-        @exportRpc('cdat.view.create')
-        def create_view(self, *arg, **kw):
-            renderer = vtk.vtkRenderer()
-            renderWindow = vtk.vtkRenderWindow()
-            renderWindow.AddRenderer(renderer)
-
-            renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-            renderWindowInteractor.SetRenderWindow(renderWindow)
-            renderWindowInteractor.GetInteractorStyle().SetCurrentStyleToTrackballCamera()
-
-            cone = vtk.vtkConeSource()
-            mapper = vtk.vtkPolyDataMapper()
-            actor = vtk.vtkActor()
-
-            mapper.SetInputConnection(cone.GetOutputPort())
-            actor.SetMapper(mapper)
-
-            renderer.AddActor(actor)
-            renderer.ResetCamera()
-            renderWindow.Render()
-            id = self.getGlobalId(renderWindow)
-            self._open_views[id] = renderWindow
-            return id
-
-        @exportRpc('cdat.view.destroy')
-        def destroy_view(self, id):
-            cache = self._open_views.pop(id, None)
-            if cache:
-                cache.Finalize()
-
-
-if __name__ == '__main__':
-    print "CDATWeb Visualization server initializing"
+    print("CDATWeb Visualization server initializing")
     server.start_webserver(options=args, protocol=CDATWebVisualizer)
