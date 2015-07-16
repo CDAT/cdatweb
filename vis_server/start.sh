6340ff8eea6a61b9b7829ee2467ee557f67293b3
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-07-16 12:48:13 -0400

    Update instructions for running a local visualization server

--- vis_server/start.sh
+++ vis_server/start.sh
@@ -34,4 +34,4 @@ rm -f ../tmp/logs/* &> /dev/null
 mkdir -p ../tmp/logs &> /dev/null
 
 # start up the server
-"${UVCDAT_SETUP_PATH}/Externals/lib/python2.7/site-packages/vtk/web/launcher.py" config.json
+"${UVCDAT_SETUP_PATH}/Externals/lib/python2.7/site-packages/vtk/web/launcher.py" config-local.json
