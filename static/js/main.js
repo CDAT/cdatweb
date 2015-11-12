3e533efb7367c22572a6108f58a6d3262dcd2434
Matthew Harris <mattbenharris@gmail.com>
2015-11-12 10:23:06 -0800

    MBH: refactored esgf search results, updated esgf search result variables to be dragable

--- static/js/main.js
+++ static/js/main.js
@@ -4,97 +4,97 @@ function cdat_esgf_submit(){
   var text =    $("input[id=text]").val();
   var project = $("input[id=project]").val();
   var limit =   document.getElementById("limit").value;
-  console.log(limit);
+  //console.log(limit);
   var offset =  $("input[id=offset]").val();
 
-    var jsonObj = new Object;
-    jsonObj.host = host;
-	  jsonObj.text = text;
-	  jsonObj.project = project;
-	  jsonObj.limit = limit;
-	  jsonObj.offset = offset;
+  var jsonObj = new Object;
+  jsonObj.host = host;
+  jsonObj.text = text;
+  jsonObj.project = project;
+  jsonObj.limit = limit;
+  jsonObj.offset = offset;
 
-	  var jsonStr = JSON.stringify(jsonObj);
-	  var search_url = '/vtk/search';
-	  $.ajax({
-	    type: "POST",
-            url: search_url,
-	    async: true,
-	    cache: false,
-  	    data: {query:jsonStr},
-  	    dataType: 'json',
-  	    success: function(data) {
-          results = data.data;
-          stacktop = [];
-          stacktop = results;
-          html = "<div><ul>"
-          for(var x = 0; x < stacktop.length; x++){
-            //console.log(stacktop[x]);
-            obj = stacktop[x];
-            /*
-            obj example
-              dap: "http://esgdata1.nccs.nasa.gov/thredds/dodsC/NEX/dataportal/NEX/downscaled/NASA-Ames/BCSD/historical/mon/atmos/pr/r1i1p1/v1.0/CONUS/pr_amon_BCSD_historical_r1i1p1_CONUS_FGOALS-g2_199501-199912.nc.html"
-              experiment: "decadal2005"
-              http: "http://adm07.cmcc.it/thredds/fileServer/esg_dataroot/cmip5/output1/CMCC/CMCC-CM/decadal2005/mon/atmos/cct/r1i1p1/cct_Amon_CMCC-CM_decadal2005_r1i1p1_200511-201512.nc"
-              id: null
-              metadata_format: null
-              node: null
-              project: "CMIP5"
-              regridding: null
-              size: 56245984
-              timestamp: "2012-03-06T23:52:36Z"
-              title: "cct_Amon_CMCC-CM_decadal2005_r1i1p1_200511-201512.nc"
-              type: null
-              urls: Array[1]
-              variables: Array[0]
-            */
-            html = html + "<li>Project: " + obj.project + "<br/>" +
-                          "Experiment: " + obj.experiment + "<br/>" +
-                          "Title: " + obj.title + "<br/>" +
-                          "<a href=\"" + obj.http + "\">Downlaod</a><br/>";
+  var jsonStr = JSON.stringify(jsonObj);
+  var search_url = '/vtk/search';
+  $.ajax({
+    type: "POST",
+    url: search_url,
+    async: true,
+    cache: false,
+    data: {query:jsonStr},
+    dataType: 'json',
+    success: function(data) {
+      results = data.data;
+      stacktop = [];
+      stacktop = results;
+      var element = $("<div></div>");
+      if(stacktop.length == 0){
+        element.text("No Results");
+      }
 
-            html = html + "Variables: ";
-            for(var v = 0; v < obj.variables.length; v++){
-              if (typeof(obj.dap) === "undefined"){
-                html = html + " " + obj.variables[v].name + "&nbsp;";
-                html = html + "Variables: ";
-                for(var v = 0; v < obj.variables.length; v++){
-                  if (typeof(obj.dap) === "undefined"){
-                    html = html + " " + obj.variables[v].name + "&nbsp;";
-                  }
-                  else{
-                    _dap = obj.dap
-                    if(_dap.substring(_dap.length - 5, _dap.length) == ".html"){
-                      _dap = _dap.substring(0, _dap.length - 5)
-                    }
-                    html = html + " <a  onclick=\"cdat.create_plot('" + _dap + "', '" + obj.variables[v].name + "')\"  href=\"javascript:void(0)\">" + obj.variables[v].name + "</a> &nbsp;";
-                  }
-                }
+      for(var x = 0; x < stacktop.length; x++){
+        //console.log(stacktop[x]);
+        obj = stacktop[x];
+        var wrapper =$("<p></p>");
+        var eol = "<br/>";
+        var split = "<hr>";
+        wrapper.append(split);
 
+        var project = "Project: " + obj.project + eol;
+        wrapper.append(project);
 
-                html = html + "</li><hr>";
-              }
-            }
+        var experiment ="Experiment: " + obj.experiment + eol;
+        wrapper.append(experiment);
+
+        var title ="Title: " + obj.title + eol;
+        wrapper.append(title);
+
+        var download = $("<a></a>");
+        download.attr("href", obj.http).text("Download");
+        wrapper.append(download);
+
+        wrapper.append(eol + "Variables: " + eol);
+        var var_wrap = $("<ul></ul>");
+        
+        if(obj.variables.length == 0){
+          var no_vars = $("<li></li>");
+          no_vars.text("No Listed Variables");
+          var_wrap.append(no_vars);
+        }
+
+        for(var v = 0; v < obj.variables.length; v++){
+          var variable = $("<li></li>");
+          if (typeof(obj.dap) === "undefined"){
+            variable.text(obj.variables[v].name);
           }
+          else{
+            _dap = obj.dap
+            if(_dap.substring(_dap.length - 5, _dap.length) == ".html"){
+              _dap = _dap.substring(0, _dap.length - 5)
+            }
+            var link = $("<a></a>");
+            make_draggable(link);
+            link.text(obj.variables[v].name)
+              .attr("data-name", obj.variables[v].name)
+              .attr("data-file", _dap)
+              .addClass('cdat-variable');
+            variable.append(link);
+          }
+          var_wrap.append(variable);
+        }
 
+        wrapper.append(var_wrap);
+        element.append(wrapper);
+      }
 
-        // cdat.create_plot(
-          //  file_or_url,
-          //  variable_or_variables, // i.e. 'U', ['U'], or ['U', 'V']
-          //  type,
-          //  method,
-          //  template
-        // )
-
-        html = html + "</ul></div>";
-        title = "Search Results";
-        newPanel(title, html);
-      },
-      error: function(request, status, error) {
-	      $("div .error").html(request + " | " + status + " | " + error);
-	      $("div .error").show();
-	    }
-      });
+      title = "Search Results";
+      newPanel(title, element);
+    },
+    error: function(request, status, error) {
+      $("div .error").html(request + " | " + status + " | " + error);
+      $("div .error").show();
+    }
+  });
 }
 
 function newPanel(title, content){
@@ -175,7 +175,7 @@ function get_children(path, parent, level){
         element.find("ul").attr("id", parent_id + "_" + display_name);
         parent.append(element);
       }
-  },
+    },
     error: function(request, status, error) {
       console.log(status + " | " + error)
     }
@@ -187,16 +187,16 @@ function get_variables(path, parent, level){
   //testing above
   cdat.get_variables(path).then(
     function (variables){
-      for(v in variables){
-        element = $("<li><a></a></li>");
-        make_draggable(element);
-        element.text(v)
-          .attr("data-name", v)
-          .attr("data-file", path)
-          .addClass('cdat-variable');
-        parent.append(element);
-      }
-    }, function(){console.log(arguments)}
+    for(v in variables){
+      element = $("<li><a></a></li>");
+      make_draggable(element);
+      element.text(v)
+      .attr("data-name", v)
+      .attr("data-file", path)
+      .addClass('cdat-variable');
+      parent.append(element);
+    }
+  }, function(){console.log(arguments)}
   )
 }
 
@@ -218,11 +218,11 @@ function make_draggable(node, ondrag) {
     addClass: "cdat-grabbing",
     opacity: 0.75
   }).addClass('cdat-draggable')
-    .on('start', function (evt) {
-      if (ondrag) {
-        ondrag.call(node, evt);
-      }
-    });
+  .on('start', function (evt) {
+    if (ondrag) {
+      ondrag.call(node, evt);
+    }
+  });
 
   return node;
 }
@@ -249,61 +249,61 @@ $("body").ready(function(){
 
   cdat.get_graphics_methods().then(
     function(plots){
-      parent = $(".cdatweb-plot-types");
-      var item = $("<li><a></a><ul class='qtree'></ul></li>");
-      var child = $("<li><a></a></li>");
-      var plot_fam_item, plot_family, plot_item;
-      for (plot_family in plots) {
-        if (plots.hasOwnProperty(plot_family) === false) {
-          continue;
-        }
-
-        plot_fam_item = item.clone();
-
-        plot_fam_item.attr('id', plot_family);
-        plot_fam_item.find('a').text(plot_family);
-
-        for(plot_type in plots[plot_family]){
-          plot_item = child.clone();
-          plot_item.attr('id', plot_type);
-          plot_item
-            .addClass('cdat-plot-method')
-            .attr('data-type', plot_type)
-            .attr('data-family', plot_family)
-            .attr('data-nvars', plots[plot_family][plot_type].nvars)
-            .text(plot_type);
-          plot_item.hide();
-          make_draggable(plot_item);
-          plot_fam_item.find("ul").append(plot_item);
-        }
-        parent.append(plot_fam_item);
-        plot_fam_item.hide();
+    parent = $(".cdatweb-plot-types");
+    var item = $("<li><a></a><ul class='qtree'></ul></li>");
+    var child = $("<li><a></a></li>");
+    var plot_fam_item, plot_family, plot_item;
+    for (plot_family in plots) {
+      if (plots.hasOwnProperty(plot_family) === false) {
+        continue;
       }
-    },
-    function(){
-      console.log(arguments)
+
+      plot_fam_item = item.clone();
+
+      plot_fam_item.attr('id', plot_family);
+      plot_fam_item.find('a').text(plot_family);
+
+      for(plot_type in plots[plot_family]){
+        plot_item = child.clone();
+        plot_item.attr('id', plot_type);
+        plot_item
+        .addClass('cdat-plot-method')
+        .attr('data-type', plot_type)
+        .attr('data-family', plot_family)
+        .attr('data-nvars', plots[plot_family][plot_type].nvars)
+        .text(plot_type);
+        plot_item.hide();
+        make_draggable(plot_item);
+        plot_fam_item.find("ul").append(plot_item);
+      }
+      parent.append(plot_fam_item);
+      plot_fam_item.hide();
     }
+  },
+  function(){
+    console.log(arguments)
+  }
   );
 
   cdat.get_templates().then(
     function(templates){
-      parent = $(".cdatweb-plot-templates");
-      var item = $("<li><a></a><ul class='qtree'></ul></li>");
-      var temp_fam_item, temp_name;
-      for(temp_name = 0; temp_name < templates.length; temp_name++) {
-        temp_fam_item = item.clone();
-        temp_fam_item.attr('id', templates[temp_name])
-          .text(templates[temp_name])
-          .addClass('cdat-template-option');
-        make_draggable(temp_fam_item);
-        parent.append(temp_fam_item);
-        temp_fam_item.hide();
-      }
+    parent = $(".cdatweb-plot-templates");
+    var item = $("<li><a></a><ul class='qtree'></ul></li>");
+    var temp_fam_item, temp_name;
+    for(temp_name = 0; temp_name < templates.length; temp_name++) {
+      temp_fam_item = item.clone();
+      temp_fam_item.attr('id', templates[temp_name])
+      .text(templates[temp_name])
+      .addClass('cdat-template-option');
+      make_draggable(temp_fam_item);
+      parent.append(temp_fam_item);
+      temp_fam_item.hide();
+    }
 
   },
-    function(){
-      console.log(arguments)
-    }
+  function(){
+    console.log(arguments)
+  }
   );
   $(".qtree").quicktree();
   $(".cdat-search-link").click(function () {
@@ -313,24 +313,24 @@ $("body").ready(function(){
 
     $(".cdat-search-container").remove();
     $("<div/>").addClass("cdat-search-container")
-      .appendTo(".vtk-view-container");
+    .appendTo(".vtk-view-container");
 
     $.ajax('/fragments/search')
-      .then(function (data) {
-        $('.cdat-search-container').html(data);
-        cdat.make_panel(
-          $('.cdat-search-container').get(0),
-          '',
-          {
-            title: '<span><i class="fa fa-search"></i>ESGF search</span>',
-            size: 'auto',
-            overflow: 'scroll'
-          }
-        );
-      }, function () {
-        console.log('search fragment load failed.');
-        $('.cdat-search-container').remove();
-      });
+    .then(function (data) {
+      $('.cdat-search-container').html(data);
+      cdat.make_panel(
+        $('.cdat-search-container').get(0),
+        '',
+        {
+          title: '<span><i class="fa fa-search"></i>ESGF search</span>',
+          size: 'auto',
+          overflow: 'scroll'
+        }
+      );
+    }, function () {
+      console.log('search fragment load failed.');
+      $('.cdat-search-container').remove();
+    });
   });
 });
 
